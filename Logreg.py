b70bd87fe32e88d7c26717e4d1c360aa8ec43856
asudheer28 <79919219+asudheer28@users.noreply.github.com>
2021-03-02 15:20:13 -0500

    Add files via upload

---
+++ Logreg.py
@@ -0,0 +1,53 @@
+
+
+import pandas as pd
+import numpy as np
+from sklearn import datasets
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder
+
+data = pd.read_excel(r'C:\Users\asudh\Downloads\PROJECT2.csv')
+
+df1 = data.copy()
+df1.drop(['Unnamed: 0'], axis = 1,inplace=True)
+
+X = df1.iloc[:,:3]
+y= df1["Status_ACTIVE"]
+
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y, test_size = 0.2,random_state=123)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+
+logreg.fit(X_train, y_train)
+y_pred = logreg.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+accuracy_score(y_test, y_pred)
+
+from sklearn.metrics import confusion_matrix
+confusion_matrix = confusion_matrix(y_test, y_pred)
+print(confusion_matrix)
+from sklearn.metrics import classification_report
+print(classification_report(y_test, y_pred))
+
+import pickle
+
+pickle.dump(logreg, open("logreg.pkl", "wb"))
+
+model = pickle.load(open("logreg.pkl", "rb"))
+
+print(model.predict([[25,50000,12]]))
+
+
+
+
+
+
+
+
